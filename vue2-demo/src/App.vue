09aa790d3a4719a504df91f1f45fed6c2fb46996
Tiancheng <yukino161043261@gmail.com>
2024-07-24 00:55:25 +0800

    Initial commit

---
+++ vue2-demo/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <AddSum v-if="show"/>
+</template>
+
+<script>
+import AddSum from "@/components/AddSum.vue";
+
+export default {
+  name: 'App',
+  components: {AddSum},
+  comments: { AddSum },
+  data() {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ vue2-demo/src/components/AddSum.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="addSum">
+    <h1>sum = {{ sum }}</h1>
+    <button @click="add">sum++</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'AddSum',
+  data() {
+    return {
+      sum: 1
+    }
+  },
+  methods: {
+    add() {
+      this.sum++;
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+
+  created() {
+    console.log('created');
+  },
+
+  beforeMount() {
+    console.log('beforeMount');
+  },
+
+  mounted() {
+    console.log('mounted');
+  },
+
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+
+  updated() {
+    console.log('updated');
+  },
+
+  beforeDestroy() {
+    console.log('beforeDestroy');
+  },
+
+  destroyed() {
+    console.log('destroyed');
+  }
+}
+</script>
+
+<style scoped>
+.addSum {
+  background-color: skyblue;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px;
+}
+</style>
