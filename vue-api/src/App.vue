1ba5b3bcb68a09c3aa36ff2b6de7a71dda677703
Tiancheng <yukino161043261@gmail.com>
2024-07-28 16:10:31 +0800

    update

---
+++ vue-api/src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+</script>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+let sum = ref(0)
+let p1 = ref({ name: 'Tomcat', age: 22 });
+
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
